f12c5d2480f9ca0b4df04d8bc11003ba274545ff
Synicpty <112263888+Synicpty@users.noreply.github.com>
2022-09-29 19:26:48 -0500

    Add files via upload

---
+++ parcial1.com/1/utils/alquila_util.php
@@ -0,0 +1,15 @@
+<?php
+
+    class alquila_util{
+
+        public static function Operaciones($tarifax, $diasx){
+
+            $preciox=   $tarifax * $diasx;
+
+            return $preciox;
+
+        }
+
+    }
+
+?>
